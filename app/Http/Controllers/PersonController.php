c69a6bbb552867fe90a9e9942341fb483424ef12
Rodrigo <rodrigorozasv@outlook.cl>
2020-09-20 10:19:05 -0300

    CRUD de person

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Person;
+
+class PersonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $persons = Person::all();
+
+        $res = [
+            'status' => 'ok',
+            'message' => 'Lista de personas',
+            'code' => 1000,
+            'data' => $persons
+        ];
+
+        return $res;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $jsonPerson = $request->json()->all();
+
+        $person = new Person($jsonPerson);
+
+        /*$person->firstName = $jsonPerson["firstName"];
+        $person->lastName = $jsonPerson["lastName"];
+        $person->documentNumber = $jsonPerson["documentNumber"];
+        $person->country = $jsonPerson["country"];
+        $person->city = $jsonPerson["city"];
+        $person->street = $jsonPerson["street"];
+        $person->number = $jsonPerson["number"];
+        $person->single = $jsonPerson["single"];*/
+
+        $person->save();
+        
+        $res = [
+            'status' => 'ok',
+            'message' => 'Persona creada',
+            'code' => 1003,
+            'data' => $person
+        ];
+
+        return $res;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $person = Person::find($id);
+
+        if (isset($person)) {
+            $res = [
+                'status' => 'ok',
+                'message' => 'Obteniendo persona por id '.$id,
+                'code' => 1001,
+                'data' => $person
+            ];
+        }else{
+            $res = [
+                'status' => 'fail',
+                'message' => 'No se encontro persona por id '.$id,
+                'code' => 1011,
+                'data' => null
+            ];
+        }
+
+        
+
+        return $res;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $person = Person::find($id);
+
+        if (isset($person)) {
+            $person->update($request->json()->all());
+
+            $res = [
+                'status' => 'ok',
+                'message' => 'Persona actualizada',
+                'code' => 1005
+            ];
+
+        }else{
+            $res = [
+                'status' => 'fail',
+                'message' => 'No se encontro persona a actualizar',
+                'code' => 1015
+            ];
+        }
+
+        return $res;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $person = Person::find($id);
+
+        if (isset($person)) {
+            $person->delete();
+
+            $res = [
+                'status' => 'ok',
+                'message' => 'Persona eliminada',
+                'code' => 1004
+            ];
+
+        }else{
+            $res = [
+                'status' => 'fail',
+                'message' => 'No se encontro persona a eliminar',
+                'code' => 1014
+            ];
+        }
+
+        return $res;
+    }
+}
---
+++ app/Person.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    protected $table = 'persons';
+
+    protected $fillable = [
+        "firstName","lastName","documentNumber",
+    "country", "city","street","number","single"
+    ];
+
+    protected $hidden = [
+        "created_at", "updated_at"
+    ];
+}
